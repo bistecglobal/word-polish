92e0d85fa1698306b2a45b8896e675ef9cebc093
Chandima Ranaweera <chan4lk@gmail.com>
2023-10-27 13:04:26 +0530

    use system prompt

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -10,12 +10,15 @@ export class OpenAiApi {
     });
   }
   // Asynchronous function to generate text from the OpenAI API
-  async generateText(prompt, model = "gpt-3.5-turbo", max_tokens = 500, temperature = 0.85) {
+  async generateText(systemPrompt, prompt, model = "gpt-3.5-turbo", max_tokens = 500, temperature = 0.85) {
     // Send a request to the OpenAI API to generate text
     try {
       const response = await this.openAI.chat.completions.create({
         model,
-        messages: [{ role: "user", content: prompt }],
+        messages: [
+          { role: "system", content: systemPrompt },
+          { role: "user", content: prompt },
+        ],
         max_tokens,
         n: 1,
         temperature,
