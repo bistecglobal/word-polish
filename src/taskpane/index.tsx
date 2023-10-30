b5d954d6c5bac8912e5a3d2007e71fe25ec931da
Chandima Ranaweera <chan4lk@gmail.com>
2023-10-30 11:38:38 +0530

    generated published version

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -11,7 +11,7 @@ initializeIcons();
 
 let isOfficeInitialized = false;
 
-const title = "Contoso Task Pane Add-in";
+const title = "Bistec Word Polish";
 
 const render = (Component) => {
   ReactDOM.render(
